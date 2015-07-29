bc4f92523bd6347a03fcda8a305193b832bffae9
Andreu <andreus.sala@gmail.com>
2015-07-29 16:01:23 +0200

    Autoload y Refactoring quiz_controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,37 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si a la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
+
 //GET /quizes
 exports.index = function(req, res){
   models.Quiz.findAll().then(function(quizes){
     res.render('quizes/index', {title: 'Lista Preguntas', quizes: quizes});
+  }).catch(function(error){
+      next(error);
   });
 };
 
+
 //GET /quizes/:id
 exports.show = function(req, res){
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-      res.render('quizes/show', {title: 'Pregunta Quiz', quiz: quiz});
-  });
-
+  res.render('quizes/show', {title: 'Pregunta Quiz', quiz: req.quiz});
 };
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res){
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-    if(req.query.respuesta === quiz.respuesta){
-      res.render('quizes/answer', {title: 'Respuesta Quiz', respuesta: 'Correcto', quiz: quiz});
-    }else{
-      res.render('quizes/answer', {title: 'Respuesta Quiz', respuesta: 'Incorrecto', quiz: quiz});
-    }
-  });
+  var resultado = 'Incorrecto';
+  if(req.query.respuesta === req.quiz.respuesta){
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {title: 'Respuesta Quiz', respuesta: resultado, quiz: req.quiz});
 };
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+//Definición de rutas de /quizes
 router.get('/quizes', quizController.index); //list of quizes
 router.get('/quizes/:quizId(\\d+)', quizController.show); //uniq quize from id
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer); //uniq answer from quiz
